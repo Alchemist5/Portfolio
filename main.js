e182c30eb9a3d1c4645778a577d73d09d6d6bb4d
Alchemist5 <josphathumphrey5@gmail.com>
2022-02-11 16:01:48 +0300

    Updated

--- main.js
+++ main.js
@@ -6,52 +6,110 @@ const signUp = document.querySelector('.sign-up');
 const modalWrapper = document.querySelector('.modal-wrapper');
 const profileWrapper = document.querySelector('.profile-wrapper');
 const headr = document.getElementById('header');
+const navbar = document.querySelector('.navbar');
+const serviceItems = document.querySelectorAll('.services-item')
+const skillItems = document.querySelectorAll('.skills-item')
+const projectItems = document.querySelectorAll('.project-item')
 
+// Sliding navbar
 let scrolled = false;
 
 window.onscroll = () => {
     if(window.pageYOffset > 100) {
+        headr.classList.remove('top');
+        navbar.style.box = " "
         if(!scrolled) {
-            headr.style.transform = 'translateY(-70px)'
+            headr.style.transform = 'translateY(-70px)';
         }
 
         setTimeout(() => {
             headr.style.transform = 'translateY(0px)';
-            headr.classList.add('top')
             scrolled = true;
         }, 500)
-    }else {
-        headr.classList.remove('top');
-        scrolled = false;
+    } else {
+            headr.classList.add('top');
+            scrolled = false;
     }
 }
-
-// profileWrapper.addEventListener('click', () => {
-//     profileWrapper.classList.toggle('show-back')
-// })
-
-header.addEventListener('click', () => {
-    // document.body.classList.toggle('show-nav');
-    sideNav.classList.add('show-nav');
-    navContainer.classList.add('nav-background')
-})
-
-menuBar.addEventListener('click', () => {
-    // document.body.classList.toggle('show-nav');
-    sideNav.classList.toggle('show-nav');
-    navContainer.classList.add('nav-background')
-})
-
-window.addEventListener('click', (e) => {
-    // if(e.target == navContainer) {
-    //     document.body.classList.remove('show-nav')
-    // }
-                  
-    e.target == navContainer ? sideNav.classList.remove('show-nav') & navContainer.classList.remove('nav-background') : false;
-})
 
 signUp.addEventListener('click', () => {
     modalWrapper.style.display = 'flex';
 })
 
 window.addEventListener('click', e => e.target == modalWrapper ? modalWrapper.style.display = 'none' : false)
+
+// Check if in viewport
+const isInViewport = (item, offset = 90) => {
+        const rectTop = item.getBoundingClientRect().top
+
+        return (
+            rectTop <= ((window.innerHeight || document.documentElement.clientHeight) - offset)
+        );
+   
+}
+
+
+const scrollAnimation = () => {
+    
+    serviceItems.forEach((item,arr) => {
+        if(isInViewport(item)) {
+            item.classList.add('show')
+        } 
+    })
+}
+    
+// Events
+window.addEventListener('load', scrollAnimation);
+window.addEventListener('resize', scrollAnimation);
+window.addEventListener('scroll', scrollAnimation);
+
+
+// Check if in viewport
+const isInViewport2 = (item, offset = 1.30) => {
+        const rectTop = item.getBoundingClientRect().top
+
+        return (
+            rectTop <= ((window.innerHeight || document.documentElement.clientHeight) / offset)
+        );
+   
+}
+
+
+const scrollAnimation2 = () => {
+    
+    skillItems.forEach(item => {
+        if(isInViewport2(item)) {
+            item.classList.add('show')
+        } 
+    })
+}
+    
+// Events
+window.addEventListener('load', scrollAnimation2);
+window.addEventListener('resize', scrollAnimation2);
+window.addEventListener('scroll', scrollAnimation2);
+
+// Check if in viewport
+const isInViewport3 = (item, offset = 1.2) => {
+        const rectTop = item.getBoundingClientRect().top
+
+        return (
+            rectTop <= ((window.innerHeight || document.documentElement.clientHeight) / offset)
+        );
+   
+}
+
+
+const scrollAnimation3 = () => {
+    
+    projectItems.forEach(item => {
+        if(isInViewport3(item)) {
+            item.classList.add('show')
+        } 
+    })
+}
+    
+// Events
+window.addEventListener('load', scrollAnimation3);
+window.addEventListener('resize', scrollAnimation3);
+window.addEventListener('scroll', scrollAnimation3);
